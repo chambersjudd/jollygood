14812e35f4865c6262e336980e44a935413f0ba5
James Chambers <james@chambersjudd.com>
2012-05-02 17:53:44 +0100

    More comprehensive text subs

--- js/jollyGood.js
+++ js/jollyGood.js
@@ -1,7 +1,7 @@
 var jollyGood 	= {};
 
-// var elementsToIgnore	= '.messageBody, .uiAttachmentDesc, .commentBody, .hasCaption, .uiStreamMessage';
-var elementsToIgnore 	= '';
+var elementsToIgnore	= '.messageBody, .uiAttachmentDesc, .commentBody, .hasCaption, .uiStreamMessage';
+// var elementsToIgnore 	= '';
 
 var adjective 		= 'Jolly Good';
 
@@ -29,24 +29,33 @@ chrome.extension.sendRequest({method: "getLocalStorage", key: "adjective"}, func
 		
 		/* Body text
 		 ------------------------------------------------- */
-		// like this
-		$("body *").not(elementsToIgnore).replaceText( /like this/gi, "This is " + adjective);
 		
-		// like this
-		$("body *").not(elementsToIgnore).replaceText( /Unlike/gi, "This is not " + adjective + "");
+		var $body 	= $("body *").not(elementsToIgnore);
 		
-		// likes this
-		$("body *").not(elementsToIgnore).replaceText( /likes this/gi, "thinks this is " + adjective);
+		// NUMBER people like COMPANY
+		$('.fbEmuContext').not('.jollied').replaceText( /people like/gi, "people think").replaceText('.', '').append(' is ' + adjective).addClass('jollied');
 		
+		// PERSON likes COMPANY
+		$body.replaceText( /[A-Z] likes [A-Z]/gi, "thinks $3 is " + adjective);
 		
-		// liked *****
-		$("body *").not(elementsToIgnore).replaceText( /liked /gi, "" + adjective + ' that ');
+		// like this page
+		$body.replaceText( /like this page/gi, "" + adjective);
 		
-		// Someone likes something - TODO
-		// $("body *").not(elementsToIgnore).replaceText( /like/gi,  adjective);
+		// person likes this
+		$body.replaceText( /likes this/gi, "thinks this is " + adjective);
 		
-		// clean up and uncaught likes
-		$("body *").not(elementsToIgnore).replaceText( /like/gi,  adjective);
+		// X people like this
+		// person and 12 other like this
+		$body.replaceText( /like this/gi, "think this is " + adjective);
+		
+		// Unlike
+		$body.replaceText( /Unlike/gi, "This is not " + adjective + "");
+		
+		// Liked
+		$body.replaceText( /liked /gi, "This is " + adjective + '');
+		
+		// like (link under a post)
+		$body.replaceText( /like/gi,  adjective);
 		
 	}
 	
